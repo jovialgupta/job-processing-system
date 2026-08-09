e2c2f9afd8e968ea70dfb81a22489d94c2bf5316
jovialgupta <jovialgupta2231@gmail.com>
2026-08-10 02:29:55 +0530

    jobs routes complete

--- backend/app/routes/jobs.py
+++ backend/app/routes/jobs.py
@@ -1,4 +1,8 @@
 from fastapi import APIRouter
+from .. import models
+from ..database import SessionLocal
+from ..schemas import JobResponse
+from fastapi import APIRouter, Depends, HTTPException
 
 router = APIRouter(
     prefix="/jobs",
@@ -7,3 +11,37 @@ router = APIRouter(
 @router.post("/")
 def create_job():
     return {"message": "Job created"}
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.post("/", response_model=JobResponse)
+def create_job(db: Session = Depends(get_db)):
+    job = models.Job(
+        filename="test.pdf",
+        status="pending"
+    )
+
+    db.add(job)
+    db.commit()
+    db.refresh(job)
+
+    return job
+
+@router.get("/{job_id}", response_model=JobResponse)
+def get_job(job_id: int, db: Session = Depends(get_db)):
+    job = db.query(models.Job).filter(models.Job.id == job_id).first()
+
+    if job is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Job not found"
+        )
+
+
+    return job
