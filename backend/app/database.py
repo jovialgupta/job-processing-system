e62be6eb1f2ec7469fc846f06aada8c63a8378e3
jovialgupta <jovialgupta2231@gmail.com>
2026-08-10 20:58:27 +0530

    api running

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine 
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:Test12345@localhost:5432/job_processing"
+DATABASE_URL = "postgresql://postgres:Test12345@localhost:5433/job_processing"
 engine=create_engine(DATABASE_URL)
 
 SessionLocal=sessionmaker(
     autocommit=False,
-    autoFlush=False,
+    autoflush=False,
     bind = engine
 )
 Base= declarative_base()
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from .database import Base
+from datetime import datetime
 
 
 class Job(Base):
@@ -8,6 +9,6 @@ class Job(Base):
     id = Column(Integer, primary_key=True, index=True)
     filename = Column(String, nullable=False)
     status = Column(String, default="QUEUED", nullable=False)
-    created_at = Column(DateTime, nullable=False)
+    created_at = Column(DateTime,default=datetime.utcnow, nullable=False)
     completed_at = Column(DateTime, nullable=True)
     result = Column(String, nullable=True)
