e317e6533cacfc454ad2b3aa2b46d6874ea9ad01
jovialgupta <jovialgupta2231@gmail.com>
2026-08-13 14:40:22 +0530

    code cleanup

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,12 +1,18 @@
-from sqlalchemy import create_engine 
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:Test12345@localhost:5433/job_processing"
-engine=create_engine(DATABASE_URL)
+load_dotenv()
 
-SessionLocal=sessionmaker(
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
-    bind = engine
+    bind=engine
 )
-Base= declarative_base()
+
+Base = declarative_base()
